14bd4f0a5f9c19cfac047e7b7a65c63cc7126b84
Ale PR <caprjournalism@gmail.com>
2026-01-28 18:53:53 -0500

    Fixing style issues in MainLayout and Home pages.

--- ClientApp/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
+++ ClientApp/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4662f76437cd47a0600888c93f9ecbc3348c00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af4edae4dd8791cd7c323ed8eeab6e0d4cedbd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServerApp/obj/Debug/net9.0/ServerApp.AssemblyInfo.cs
+++ ServerApp/obj/Debug/net9.0/ServerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4662f76437cd47a0600888c93f9ecbc3348c00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af4edae4dd8791cd7c323ed8eeab6e0d4cedbd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
