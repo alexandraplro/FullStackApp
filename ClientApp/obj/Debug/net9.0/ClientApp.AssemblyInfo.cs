50fb24f3f4c6312c9acdbfd6484c6c621f056c89
Ale PR <caprjournalism@gmail.com>
2026-02-03 00:03:56 -0500

    Troubleshooting dashboard and deliveries page rendering issues

--- ClientApp/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
+++ ClientApp/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539ee1b020c044c47fc01e3e567baed7da8fc2c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a559654e457a68e06751de382012178e0f6a6c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
