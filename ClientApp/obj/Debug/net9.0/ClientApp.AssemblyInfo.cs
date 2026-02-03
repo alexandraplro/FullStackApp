539ee1b020c044c47fc01e3e567baed7da8fc2c0
Ale PR <caprjournalism@gmail.com>
2026-02-02 23:52:58 -0500

    Edited the following files to improve navigation and sorting functionality in the dashboard and deliveries/losses pages.

--- ClientApp/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
+++ ClientApp/obj/Debug/net9.0/ClientApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c343a970aea2967a11e6794558b2f45068cf4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b3adb725aff68cb46d4ad2ba0b9384914528af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
